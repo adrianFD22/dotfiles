4be42214e0fded785031179c96a5a21cb55cbe8c
adrianFD22 <adrianfd22399@gmail.com>
2023-07-11 23:21:58 +0200

    Add mimeapps.list

---
+++ .config/easy_mimeapps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# $1: minor type
+# $2: default desktop entry
+create-mimetype-rules () {
+	mime_list_path="/usr/share/mime";
+
+	minor_type=$1
+	desktop_entry=$2
+
+	major_types=$(ls "$mime_list_path/$1")
+	
+	for curr_major_type in $major_types; do
+		echo "$minor_type/${curr_major_type%.*}=$desktop_entry;"
+	done
+}
+
+# Set default desktop entry 
+create-mimetype-rules text nvim.desktop >> ~/.config/mimeapps.list 
