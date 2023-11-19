cf4a2b9086bb3d4337327293672bba256546a3cb
adrianFD22 <adrianfd22399@gmail.com>
2023-11-19 10:27:47 +0100

    After BYMAT

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -122,15 +122,16 @@ config.source("gruvbox.py")
 config.set('tabs.position', 'right')
 
 # Dark mode
-c.colors.webpage.preferred_color_scheme = 'dark'
-c.colors.webpage.darkmode.enabled = True
-c.colors.webpage.darkmode.algorithm = "lightness-cielab"
-c.colors.webpage.darkmode.threshold.text = 150
-c.colors.webpage.darkmode.threshold.background = 100
-#c.colors.webpage.darkmode.policy.images = 'always'
-#c.colors.webpage.darkmode.grayscale.images = 0.35
+c.colors.webpage.preferred_color_scheme         =  'dark'
+c.colors.webpage.darkmode.enabled               =  True
+c.colors.webpage.darkmode.algorithm             =  "lightness-cielab"
+c.colors.webpage.darkmode.threshold.text        =  150
+c.colors.webpage.darkmode.threshold.background  =  100
+c.colors.webpage.darkmode.policy.images         =  'always'
+c.colors.webpage.darkmode.grayscale.images      =  0.35
+c.colors.webpage.bg                             = '#282828'
 
-config.set('colors.webpage.bg', '#282828') # Avoid qutebrowser white flashes
+#config.set('colors.webpage.bg', '#282828') # Avoid qutebrowser white flashes
 
 # Clipboard
 config.set('content.javascript.clipboard', 'access', 'github.com')
